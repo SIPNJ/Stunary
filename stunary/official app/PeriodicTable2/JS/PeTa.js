d1bd1ee6ddcd4d6cc5cdd84c5ce5379360b01d3d
SIPNJ <41440504+SIPNJ@users.noreply.github.com>
2020-08-08 20:30:41 +0700

    pointer-events

---
+++ stunary/official app/PeriodicTable2/JS/PeTa.js
@@ -0,0 +1,10 @@
+let overlay = document.getElementsByClassName("overlay")[0];
+function conte(thing){
+    console.log(thing);
+    // let element = document.getElementById(thing.id);
+    overlay.classList.toggle("darken-background");
+}
+
+overlay.addEventListener("click", function(){
+    overlay
+})
