9efbcc2187ecb5c3391cc156e6a35228b3f64d80
SIPNJ <41440504+SIPNJ@users.noreply.github.com>
2020-08-09 21:53:31 +0700

    firebase

--- stunary/official app/PeriodicTable2/JS/PeTa.js
+++ stunary/official app/PeriodicTable2/JS/PeTa.js
@@ -1,10 +1,23 @@
 let overlay = document.getElementsByClassName("overlay")[0];
+let infoPanel = document.getElementsByClassName("infoPanel")[0];
 function conte(thing){
     console.log(thing);
     // let element = document.getElementById(thing.id);
     overlay.classList.toggle("darken-background");
+    infoPanel.classList.toggle("openPanel");
 }
-
 overlay.addEventListener("click", function(){
-    overlay
+    overlay.classList.toggle("darken-background");
+    infoPanel.classList.toggle("openPanel");
+})
+
+// const key = "yGg3vfUNjYISO6uyxB3ofg2pE7xXsvkA";
+fetch(`https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Sodium`)
+.then(response => response.json())
+.then((data)=>{
+    let html_code = data["parse"]["text"]["*"];
+    let parser = new DOMParser();
+    let html = parser.parseFromString(html_code, "text/html");
+    let tables = html.querySelectorAll(".infobox");
+    console.log(tables);
 })
