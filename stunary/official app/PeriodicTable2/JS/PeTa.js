dbd3debe7b7ef77db075703ecce8394e4321da09
SIPNJ <41440504+SIPNJ@users.noreply.github.com>
2020-08-10 18:11:49 +0700

    stunary update all source js and css

--- stunary/official app/PeriodicTable2/JS/PeTa.js
+++ stunary/official app/PeriodicTable2/JS/PeTa.js
@@ -21,14 +21,14 @@ overlay.addEventListener("click", function () {
 });
 
 // const key = "yGg3vfUNjYISO6uyxB3ofg2pE7xXsvkA";
-fetch(
-  `https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Sodium`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    let html_code = data["parse"]["text"]["*"];
-    let parser = new DOMParser();
-    let html = parser.parseFromString(html_code, "text/html");
-    let tables = html.querySelectorAll(".infobox");
-    console.log(tables);
-  });
+// fetch(
+//   `https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Sodium`
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     let html_code = data["parse"]["text"]["*"];
+//     let parser = new DOMParser();
+//     let html = parser.parseFromString(html_code, "text/html");
+//     let tables = html.querySelectorAll(".infobox");
+//     console.log(tables);
+//   });
