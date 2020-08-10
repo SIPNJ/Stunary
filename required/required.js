203e13ef3f2fff0c03d826b0971eecba93a89c36
SIPNJ <41440504+SIPNJ@users.noreply.github.com>
2020-08-10 15:52:41 +0700

    move required files

---
+++ required/required.js
@@ -0,0 +1,9 @@
+let toggleLight = document.getElementById("toggle-light");
+toggleLight.addEventListener("click", function () {
+  toggleLight.classList.toggle("toggle-dark");
+  if (toggleLight.classList.contains("toggle-dark") == true) {
+    document.body.style.backgroundColor = rgb(0, 0, 0);
+  } else {
+    document.body.style.backgroundColor = rgb(255, 255, 255);
+  }
+});
--- stunary/official app/PeriodicTable2/JS/PeTa.js
+++ stunary/official app/PeriodicTable2/JS/PeTa.js
@@ -1,23 +1,34 @@
 let overlay = document.getElementsByClassName("overlay")[0];
-let infoPanel = document.getElementsByClassName("infoPanel")[0];
-function conte(thing){
-    console.log(thing);
-    // let element = document.getElementById(thing.id);
-    overlay.classList.toggle("darken-background");
-    infoPanel.classList.toggle("openPanel");
+let infoPanelforElement = document.getElementsByClassName("infoPanel")[0];
+let infoPanelforGroup = document.getElementsByClassName("infoPanel")[1];
+function elementInfo(thing) {
+  console.log(thing);
+  // let element = document.getElementById(thing.id);
+  overlay.classList.toggle("darken-background");
+  infoPanelforElement.classList.toggle("openPanel");
 }
-overlay.addEventListener("click", function(){
+function groupInfo(thing) {
+  overlay.classList.toggle("darken-background");
+  infoPanelforGroup.classList.toggle("openPanel");
+}
+overlay.addEventListener("click", function () {
     overlay.classList.toggle("darken-background");
-    infoPanel.classList.toggle("openPanel");
-})
+  if (infoPanelforElement.classList.contains("openPanel")) {
+    infoPanelforElement.classList.toggle("openPanel");
+  } else if (infoPanelforGroup.classList.contains("openPanel")) {
+    infoPanelforGroup.classList.toggle("openPanel");
+  }
+});
 
 // const key = "yGg3vfUNjYISO6uyxB3ofg2pE7xXsvkA";
-fetch(`https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Sodium`)
-.then(response => response.json())
-.then((data)=>{
+fetch(
+  `https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Sodium`
+)
+  .then((response) => response.json())
+  .then((data) => {
     let html_code = data["parse"]["text"]["*"];
     let parser = new DOMParser();
     let html = parser.parseFromString(html_code, "text/html");
     let tables = html.querySelectorAll(".infobox");
     console.log(tables);
-})
+  });
